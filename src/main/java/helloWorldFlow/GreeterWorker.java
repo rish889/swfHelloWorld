597d464ddff7500a435fa38316f2eab3c0ca712e
Garcha <rishabhg@c4b301c2b0a7.ant.amazon.com>
2018-01-22 11:55:24 +0530

    flow

--- src/main/java/helloWorldFlow/GreeterWorker.java
+++ src/main/java/helloWorldFlow/GreeterWorker.java
@@ -1,16 +1,16 @@
 package helloWorldFlow;
 
-
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflow;
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflowClient;
+import com.amazonaws.services.simpleworkflow.flow.ActivityWorker;
+import com.amazonaws.services.simpleworkflow.flow.WorkflowWorker;
 
-public class GreeterMain {
-
+public class GreeterWorker  {
     public static void main(String[] args) throws Exception {
-        ClientConfiguration config = new ClientConfiguration().withSocketTimeout(70 * 1000);
+        ClientConfiguration config = new ClientConfiguration().withSocketTimeout(70*1000);
 
         String swfAccessId = System.getenv("AWS_ACCESS_KEY_ID");
         String swfSecretKey = System.getenv("AWS_SECRET_KEY");
@@ -20,9 +20,14 @@ public class GreeterMain {
         service.setEndpoint("https://swf.us-east-1.amazonaws.com");
 
         String domain = "helloWorldWalkthrough";
+        String taskListToPoll = "HelloWorldList";
 
-        GreeterWorkflowClientExternalFactory factory = new GreeterWorkflowClientExternalFactoryImpl(service, domain);
-        GreeterWorkflowClientExternal greeter = factory.getClient("someID");
-        greeter.greet();
+        ActivityWorker aw = new ActivityWorker(service, domain, taskListToPoll);
+        aw.addActivitiesImplementation(new GreeterActivitiesImpl());
+        aw.start();
+
+        WorkflowWorker wfw = new WorkflowWorker(service, domain, taskListToPoll);
+        wfw.addWorkflowImplementationType(GreeterWorkflowImpl.class);
+        wfw.start();
     }
 }
--- src/main/java/helloWorldFlow/GreeterWorkflowImpl.java
+++ src/main/java/helloWorldFlow/GreeterWorkflowImpl.java
@@ -1,8 +1,6 @@
 package helloWorldFlow;
 
 import com.amazonaws.services.simpleworkflow.flow.core.Promise;
-import helloWorldFlow.GreeterActivitiesClient;
-import helloWorldFlow.GreeterActivitiesClientImpl;
 
 public class GreeterWorkflowImpl implements GreeterWorkflow {
     private GreeterActivitiesClient operations = new GreeterActivitiesClientImpl();
