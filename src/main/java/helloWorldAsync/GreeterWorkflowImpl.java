24fa242db789bbb7aaf3970dfd5133fcab508488
Garcha <rishabhg@c4b301c2b0a7.ant.amazon.com>
2018-01-22 12:07:54 +0530

    async

---
+++ src/main/java/helloWorldAsync/GreeterWorkflowImpl.java
@@ -0,0 +1,15 @@
+package helloWorldAsync;
+
+import com.amazonaws.services.simpleworkflow.flow.core.Promise;
+import helloWorldFlow.GreeterActivitiesClient;
+import helloWorldFlow.GreeterActivitiesClientImpl;
+
+public class GreeterWorkflowImpl implements GreeterWorkflow {
+    private GreeterActivitiesClient operations = new GreeterActivitiesClientImpl();
+
+    public void greet() {
+        Promise<String> name = operations.getName();
+        Promise<String> greeting = operations.getGreeting(name);
+        operations.say(greeting);
+    }
+}
